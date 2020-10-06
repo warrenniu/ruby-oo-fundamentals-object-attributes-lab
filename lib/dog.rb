a071157ba002828864050ab6fd4cc01782beaddd
warrenniu <warrenniu@gmail.com>
2020-10-06 11:27:38 -0400

    Completed

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require 'pry'
+class Dog
+    def name=(dogs_name)
+        @name = dogs_name
+    end
+
+    def breed=(dogs_breed)
+        @breed = dogs_breed
+    end
+    
+
+    def name
+        @name 
+    end
+
+    def breed
+        @breed
+    end
+     
+    
+    
+end
